3bc381d7daabb707b74fda31bd68c31c386979f4
AJEET VERMA <48859014+verma-ajeet@users.noreply.github.com>
2023-03-23 20:30:59 +0530

    2d matrix
    search in 2d array
    shift 2d array

--- src/twoDArray/SearchIn2DArray.java
+++ src/twoDArray/SearchIn2DArray.java
@@ -1,4 +1,9 @@
 package twoDArray;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 // starting from two right corner, coming down check if greater or not
 // if greater move towards left in that column,
 // if not greater move downwards in that row.
@@ -13,6 +18,21 @@ public class SearchIn2DArray {
 		int search=42;
         int row=0;
         int col=arr[0].length-1;
+        List<int[]> asList2 = Arrays.asList(arr);
+        for (int[] strings : asList2) {
+            for (int s : strings) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+
+        List<List<Integer>> asList = (List)Arrays.asList(arr);
+//       for (List<Integer> strings : asList) {
+//           for (int s : strings) {
+//               System.out.print(s + " ");
+//           }
+//           System.out.println();
+//       }
         while(row<arr.length && col>0) {
         	if(search==arr[row][col]) {
         		System.out.println("row: "+row+" col: "+col +" value: "+arr[row][col]);
@@ -26,5 +46,7 @@ public class SearchIn2DArray {
         		row++;
         	}
         }
+
+
 	}
 }
---
+++ src/twoDArray/Shift2DGrid.java
@@ -0,0 +1,42 @@
+package twoDArray;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Shift2DGrid {
+public static void main(String[] args) {
+	int[][] arr = {
+			{ 11, 12, 13, 14, 15, 16, 17 },
+			{ 21, 22, 23, 24, 25, 26, 27 },
+			{ 31, 32, 33, 34, 35, 36, 37 },
+			{ 41, 42, 43, 44, 45, 46, 47 },
+			{ 51, 52, 53, 54, 55, 56, 57 } };
+	List<int[]> shiftGrid = shiftGrid(arr, 2);
+	for(int[] arrr:shiftGrid) {
+		for(Integer val : arrr) {
+			System.out.print(val+" ");
+		}
+		System.out.println();
+	}
+}
+public static List<int[]> shiftGrid(int[][] grid, int k) {
+    int row=grid.length,col=grid[0].length;
+            k%=(row*col);
+    int[][] ans=new int[row][col];
+    for(int r=0;r<row;r++){
+        for(int c=0;c<col;c++){
+            int nrow=r,ncol=c+k;
+            if(ncol>=col){
+                nrow+=ncol/col;
+
+                if(nrow>=row){
+                    nrow%=row;
+                }
+                ncol%=col;
+            }
+            ans[nrow][ncol]=grid[r][c];
+        }
+    }
+    return Arrays.asList(ans);
+}
+}
